101fe9fdc81f5f02598a5af94246a5b460affcf0
Andre Weissflog <floooh@gmail.com>
2023-03-12 11:39:58 +0100

    implement a little sokol+cimgui demo

--- fibs.ts
+++ fibs.ts
@@ -1,73 +1,112 @@
 import * as fibs from 'https://raw.githubusercontent.com/floooh/fibs/main/mod.ts'
 //import * as fibs from '../fibs/mod.ts';
 
+// a fibs project is defined by an export called 'projectDesc' of type 'fibs.ProjectDesc':
 export const projectDesc: fibs.ProjectDesc = {
     name: 'fibs-test',
 
-    // define a couple of remote imports
-    imports: {
-        sokol: {
-            url: 'https://github.com/floooh/sokol',
-            projectDesc: {
-                targets: {
-                    // header-only libs are declared as 'void' targets,
-                    // such targets may define include directories, compile options
-                    // etc... for other targets
-                    sokol: { type: 'void', includeDirectories: [ '.', './util' ] }
-                }
-            }
-        },
-        imgui: {
-            url: 'https://github.com/ocornut/imgui',
-            ref: 'v1.88',
-            // an import can either have a fibs.ts file in its root directory, or
-            // we can just provide an 'inline' project description here,
-            // or without both an import can also just provide a couple of files
-            // from a remote source
-            projectDesc: {
-                targets: {
-                    imgui: {
-                        type: 'lib',
-                        sources: [
-                            'imgui_demo.cpp',
-                            'imgui_draw.cpp',
-                            'imgui_tables.cpp',
-                            'imgui_widgets.cpp',
-                            'imgui.cpp',
-                            'imgui.h',
-                        ],
-                        includeDirectories: [ '.' ]
-                    }
-                },
-                // imports can also provide their own fibs commands, e.g.
-                // this adds a 'fibs hello-imgui' subcommand:
-                commands: {
-                    'hello-imgui': {
-                        help: () => {
-                            fibs.log.help([ 'hello-imgui'], 'an example command from an import');
-                        },
-                        run: async (project: fibs.Project) => {
-                            fibs.log.print(`Hello from imgui (args: ${Deno.args.join(',')})`);
-                        }
-                    }
-                }
-            }
+    // the only target in the project is an executable 'hello' which depends  on sokol and cimgui:
+    targets: {
+        hello: {
+            type: 'windowed-exe',
+            dir: 'src',
+            sources: ['hello.c'],
+            libs: ['sokol', 'cimgui'],
         },
     },
 
-    targets: {
-        hello: {
-            type: 'plain-exe',
-            dir: 'src',
-            sources: ['hello.c'],
-            libs: ['sokol', 'print', 'imgui'],
+    // external dependencies can be pulled via git, but their project description
+    // can be provided 'inline':
+    imports: {
+        sokol: {
+            url: 'https://github.com/floooh/sokol',
+            projectDesc: sokolDesc(),
         },
-        // a simple local library
-        print: {
-            type: 'lib',
-            dir: 'src/lib',
-            sources: ['print.c', 'print.h'],
-            includeDirectories: ['.'],
+        cimgui: {
+            url: 'https://github.com/cimgui/cimgui',
+            ref: 'v1.89.3',
+            projectDesc: cimguiDesc(),
         },
     }
+}
+
+function cimguiDesc(): fibs.ProjectDesc {
+    return {
+        targets: {
+            cimgui: {
+                type: 'lib',
+                sources: [
+                    'cimgui.cpp',
+                    'cimgui.h',
+                    'imgui/imgui_demo.cpp',
+                    'imgui/imgui_draw.cpp',
+                    'imgui/imgui_tables.cpp',
+                    'imgui/imgui_widgets.cpp',
+                    'imgui/imgui.cpp',
+                    'imgui/imgui.h',
+                ],
+                includeDirectories: [ '.' ]
+            }
+        }
+    }
+}
+
+function sokolDesc(): fibs.ProjectDesc {
+    return {
+        targets: {
+            // header-only libs are declared as 'void' targets,
+            // such targets may define include directories, compile options
+            // etc... for other targets
+            sokol: {
+                type: 'void',
+                includeDirectories: [ '.', './util' ],
+                compileDefinitions: (context) => {
+                    switch (context.config.platform) {
+                        case 'windows':
+                            return [ 'SOKOL_D3D11' ];
+                        case 'macos':
+                        case 'ios':
+                            return [ 'SOKOL_METAL' ];
+                        case 'emscripten':
+                        case 'android':
+                            return [ 'SOKOL_GLES3' ];
+                        default:
+                            return [ 'SOKOL_GLCORE33' ];
+                    }
+                },
+                compileOptions: (context) => {
+                    switch (context.config.platform) {
+                        case 'macos':
+                        case 'ios':
+                            return ['-ObjC'];
+                        default:
+                            return [];
+                    }
+                },
+                linkOptions: (context) => {
+                    switch (context.config.platform) {
+                        case 'emscripten':
+                            return [ '-s', 'USE_WEBGL2=1'];
+                        default:
+                            return [];
+                    }
+                },
+                libs: (context) => {
+                    switch (context.config.platform) {
+                        case 'macos':
+                            return [
+                                '-framework Foundation',
+                                '-framework Cocoa',
+                                '-framework QuartzCore',
+                                '-framework Metal',
+                                '-framework MetalKit',
+                                '-framework AudioToolbox',
+                            ];
+                        default:
+                            return [];
+                    }
+                }
+            }
+        }
+    };
 }
--- src/lib/print.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-void print(const char* msg) {
-    printf("%s\n", msg);
-}
