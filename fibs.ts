ddffd709850070d501587dd407ccf6a807df9038
Andre Weissflog <floooh@gmail.com>
2023-03-13 18:30:47 +0100

    updated

--- fibs.ts
+++ fibs.ts
@@ -19,6 +19,15 @@ export const projectDesc: fibs.ProjectDesc = {
         }
     },
 
+    // on Emscripten, use our custom shell.html for all exe targets
+    linkOptions: (context) => {
+        if (context.config.platform === 'emscripten') {
+            return [ '--shell-file', '@root/src/shell.html' ];
+        } else {
+            return [];
+        }
+    },
+
     // external dependencies can be pulled via git, but their project description
     // can be provided 'inline' (alternatively they can reside in a 'fibs.ts' file
     // in the pulled git repo)
@@ -114,10 +123,7 @@ function sokolDesc(): fibs.ProjectDesc {
                     interface: (context) => {
                         switch (context.config.platform) {
                             case 'emscripten':
-                                return [
-                                    '-s', 'USE_WEBGL2=1',
-                                    '--shell-file', '@root/src/shell.html',
-                                ];
+                                return [ '-sUSE_WEBGL2=1', "-sMALLOC='emmalloc'" ];
                             default:
                                 return [];
                         }
