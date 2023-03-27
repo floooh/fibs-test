4985da492371a6ed33483bdadb794c0d816b7921
Andre Weissflog <floooh@gmail.com>
2023-03-27 16:48:07 +0200

    fix oopsie

--- fibs.ts
+++ fibs.ts
@@ -1,5 +1,5 @@
-//import * as fibs from 'https://raw.githubusercontent.com/floooh/fibs/main/mod.ts'
-import * as fibs from '../fibs/mod.ts';
+import * as fibs from 'https://raw.githubusercontent.com/floooh/fibs/main/mod.ts'
+//import * as fibs from '../fibs/mod.ts';
 
 export const project: fibs.ProjectDesc = {
     name: 'fibs-test',
