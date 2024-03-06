74d1f2a2036391af936a3f0829f4131fda06b2ce
Andre Weissflog <floooh@gmail.com>
2024-03-06 12:36:26 +0100

    update for latest sokol changes

--- hello.c
+++ hello.c
@@ -13,7 +13,7 @@ static struct {
 
 static void init(void) {
     sg_setup(&(sg_desc){
-        .context = sapp_sgcontext(),
+        .environment = sglue_environment(),
         .logger.func = slog_func,
     });
     simgui_setup(&(simgui_desc_t){0});
@@ -38,7 +38,7 @@ static void frame(void) {
     igColorEdit3("Background", &state.pass_action.colors[0].clear_value.r, ImGuiColorEditFlags_None);
     igEnd();
 
-    sg_begin_default_pass(&state.pass_action, sapp_width(), sapp_height());
+    sg_begin_pass(&(sg_pass){ .action = state.pass_action, .swapchain = sglue_swapchain() });
     simgui_render();
     sg_end_pass();
     sg_commit();
